249d3ad2e2ecebc2d8237bbdd36bbfc4e6bdf88c
Alona Kolesnikova <kolesnikova.alyona@gmail.com>
2016-07-12 10:18:32 +0300

    First commit

---
+++ src/main/java/LoopVerification.java
@@ -0,0 +1,9 @@
+
+public class LoopVerification {
+    public static void main(String args[]){
+        int k, y = 1;
+        for(k = 6; k >= 3; k--)
+            y = y + k;
+        System.out.println("y = " + y + "\n" + "k = " + k);
+    }
+}
---
+++ src/test/java/LoopVerificationTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shtrih on 11.07.16.
+ */
+public class LoopVerificationTest {
+    @org.junit.Test
+
+    public void main() throws Exception {
+        LoopVerification LoopVerification = new LoopVerification();
+
+        assertEquals(19, 19);
+        assertEquals(2, 2);
+    }
+}
